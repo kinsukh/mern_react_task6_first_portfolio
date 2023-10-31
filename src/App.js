56c67a1a8cddbfdb3cb02e7b583d0dd9fce75d1c
Kinsukh S. Sonbawne <95238196+kinsukh@users.noreply.github.com>
2023-10-31 16:26:16 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import About from './components/about/About';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header/> 
       <main className='main'>
         <Home/>
+        {/* <About /> */}
       </main>
     </>
   )
--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -57,8 +57,6 @@ const Data = () => {
         </h1>
         <h3 className="home__subtitle">Full-Stack Developer</h3>
         <p className="home__description">
-            I design and develop services for customers of all sizes,
-            specializing in creating stylish, modern websites, web services and online stores.
             My passion is to design digital user experiences through the bold interface and meaningful interactions.
         </p>
 
